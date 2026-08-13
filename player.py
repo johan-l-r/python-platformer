6a797508997ccf4f427ebe30ec18eacdacf7a8b8
johan-l-r <188136149+johan-l-r@users.noreply.github.com>
2026-08-12 23:03:17 -0500

    feat(player); add vector based movement

--- player.py
+++ player.py
@@ -5,16 +5,18 @@ pg.init()
 class Player: 
   def __init__(self) -> None:
     self.SIZE: int = 64
-    self.SPEED: int = 2
+    self.SPEED: int = 1
 
     self.GRAVITY: float = 0.2
     self.vectical_speed: float = 0
-    self.JUMP_FORCE: int = 12
+    self.JUMP_FORCE: int = 10
 
     self.is_on_ground: bool = False
 
     self.position: pg.math.Vector2 = pg.math.Vector2(0, 0)
-    self.direction: pg.math.Vector2 = pg.math.Vector2(0, 1)
+    self.direction: pg.math.Vector2 = pg.math.Vector2(0, 0)
+    self.acceleration: pg.math.Vector2 = pg.math.Vector2(0, 0) 
+    self.velocity: pg.math.Vector2 = pg.math.Vector2(0, 0)
 
     self.rect: pg.Rect = pg.Rect(
       self.position.x, 
@@ -24,49 +26,36 @@ class Player:
     )
 
   def move(self, keys): 
+    # apply gravity constantly 
+    self.acceleration = pg.math.Vector2(0, self.GRAVITY)
     self.direction.x = 0
 
     if keys[pg.K_a]: 
       self.direction.x = -1
-    if keys[pg.K_d]: 
+    elif keys[pg.K_d]: 
       self.direction.x = 1
+    else: 
+      self.direction.x = 0
 
-    self.position.x += self.SPEED * self.direction.x
-    self.rect.x = self.position.x
+    self.acceleration.x = self.SPEED * self.direction.x
+    self.acceleration.x -= self.velocity.x
+
+    self.velocity += self.acceleration
+    self.position += self.velocity
+
+    self.rect.bottomleft = self.position
 
   def jump(self, keys): 
-    if keys[pg.K_SPACE] and self.is_on_ground: 
+    if keys[pg.K_SPACE] and self.is_on_ground:
+      self.velocity.y = -self.JUMP_FORCE
       self.is_on_ground = False
-      self.direction.y = -1
-
-      self.vectical_speed = self.JUMP_FORCE
-
-    if not self.is_on_ground: 
-      if self.direction.y == -1: 
-        self.vectical_speed -= self.GRAVITY
-
-        if self.vectical_speed <= 0: 
-          self.direction.y = 1
-          self.vectical_speed = 0
-      
-      if self.direction.y == 1: 
-        self.vectical_speed += self.GRAVITY
-
-    else: 
-      self.direction.y = 0
-      self.vectical_speed = 0
-
-    self.position.y += self.vectical_speed * self.direction.y
-    self.rect.y = self.position.y
 
   def collide(self): 
-    if self.position.y >= 505: # provisional logic 
+    if self.position.y >= 500: # provisional logic 
       self.is_on_ground = True
 
-      self.direction.y = 0
-
-      self.position.y = 505
-      self.rect.y = self.position.y
+      self.position.y = 500
+      self.rect.bottomleft = self.position
 
   def update(self): 
     keys = pg.key.get_pressed()
